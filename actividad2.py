7ae12463186877a787270e1f3b4270f53b5d7eec
wendypromax <wendymora525@gmail.com>
2025-03-20 16:37:45 -0500

    Update actividad2.py

--- actividad2.py
+++ actividad2.py
@@ -16,11 +16,11 @@ caracteristicas(variables)
 
 
 '''
-estado_civil = input("ingresa tu estado civil:(soltero/casado/comprometido)" )
+stado_civil = input("ingresa tu estado civil:(soltero/casado/comprometido)" )
 edad = int(input("ingresa tu edad:"))
 temperamento = input("ingresa tu temperamento:(buena persona/mala persona)")
 fisico = input("ingresa tu fisico:(lindo/a/feo/a)")
-salario= floatestado_civil = input("Ingrese su estado civil (soltero/casado/comprometido): ").lower()
+salario= float (input("Ingresa tu salario: "))
 
 
 if estado_civil == "casado" or estado_civil == "comprometido":
@@ -31,7 +31,7 @@ elif temperamento == "mala persona":
     print("no puedes acercarte a esta persona, porque te genera stress")
 elif fisico == "feo":
         print("no puedes casarte con esa persona, porque no te atrae fisicamente")
-elif salario > 200.000 :
+elif salario < 200.000 :
         print("no puedes casarte con esa persona, porque no tiene suficiente dinero")
 
 else:
